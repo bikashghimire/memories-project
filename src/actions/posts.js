74c5d093041af4eaaf1d955a15c82a79429d2bb5
bikashghimire <bikashghimire2610@gmail.com>
2022-09-04 13:44:43 +0300

    implemented redux

---
+++ src/actions/posts.js
@@ -0,0 +1,9 @@
+import * as api from "../api";
+
+// Action creators
+export const getPosts = () => async (dispatch) => {
+  try {
+  } catch (error) {}
+  const action = { type: "FETCH_ALL", payload: [] };
+  dispatch(action);
+};
